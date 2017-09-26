4bd87141a5d708fafaf29542e11bcdf0180837bd
Dominik Weber <accounts@dominik-weber.name>
2017-09-26 09:52:24 +0200

    Add Test for GetMarketSummaries to not throw an Exception

---
+++ Tests/IntegrationTests/BittrexTests.cs
@@ -0,0 +1,22 @@
+﻿using BittrexSharp;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.IntegrationTests
+{
+    [TestClass]
+    public class BittrexTests
+    {
+        [TestMethod]
+        public void GetMarketSummaries_ShouldNotThrowException()
+        {
+            var bittrex = new Bittrex();
+            Func<Task> action = async () => { var _ = await bittrex.GetMarketSummaries(); };
+            action.ShouldNotThrow();
+        }
+    }
+}
