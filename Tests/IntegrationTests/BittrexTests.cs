41813402be374a527c7c355ca3cc73d3811b25c5
Dominik Weber <accounts@dominik-weber.name>
2017-10-29 14:27:55 +0100

    Add Tests for all public Api Methods

--- Tests/IntegrationTests/BittrexTests.cs
+++ Tests/IntegrationTests/BittrexTests.cs
@@ -1,4 +1,5 @@
 ﻿using BittrexSharp;
+using BittrexSharp.Domain;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -11,6 +12,33 @@ namespace Tests.IntegrationTests
     [TestClass]
     public class BittrexTests
     {
+        #region Public Api
+        private const string DefaultMarketName = "BTC-ETH";
+
+        [TestMethod]
+        public void GetMarkets_ShouldNotThrowException()
+        {
+            var bittrex = new Bittrex();
+            Func<Task> action = async () => { var _ = await bittrex.GetMarkets(); };
+            action.ShouldNotThrow();
+        }
+
+        [TestMethod]
+        public void GetSupportedCurrencies_ShouldNotThrowException()
+        {
+            var bittrex = new Bittrex();
+            Func<Task> action = async () => { var _ = await bittrex.GetSupportedCurrencies(); };
+            action.ShouldNotThrow();
+        }
+
+        [TestMethod]
+        public void GetTicker_ShouldNotThrowException()
+        {
+            var bittrex = new Bittrex();
+            Func<Task> action = async () => { var _ = await bittrex.GetTicker(DefaultMarketName); };
+            action.ShouldNotThrow();
+        }
+
         [TestMethod]
         public void GetMarketSummaries_ShouldNotThrowException()
         {
@@ -18,5 +46,30 @@ namespace Tests.IntegrationTests
             Func<Task> action = async () => { var _ = await bittrex.GetMarketSummaries(); };
             action.ShouldNotThrow();
         }
+
+        [TestMethod]
+        public void GetMarketSummary_ShouldNotThrowException()
+        {
+            var bittrex = new Bittrex();
+            Func<Task> action = async () => { var _ = await bittrex.GetMarketSummary(DefaultMarketName); };
+            action.ShouldNotThrow();
+        }
+
+        [TestMethod]
+        public void GetOrderBook_ShouldNotThrowException()
+        {
+            var bittrex = new Bittrex();
+            Func<Task> action = async () => { var _ = await bittrex.GetOrderBook(DefaultMarketName, OrderType.Both, 1); };
+            action.ShouldNotThrow();
+        }
+
+        [TestMethod]
+        public void GetMarketHistory_ShouldNotThrowException()
+        {
+            var bittrex = new Bittrex();
+            Func<Task> action = async () => { var _ = await bittrex.GetMarketHistory(DefaultMarketName); };
+            action.ShouldNotThrow();
+        }
+        #endregion
     }
 }
