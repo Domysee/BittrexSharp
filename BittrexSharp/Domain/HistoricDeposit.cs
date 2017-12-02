3cbcf36a5a5c8836c415e411e8004729a5664391
artem_kononov <admin@finbri.com>
2017-12-02 04:19:12 -0500

    fix HistoricDeposit type

--- BittrexSharp/Domain/HistoricDeposit.cs
+++ BittrexSharp/Domain/HistoricDeposit.cs
@@ -1,21 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BittrexSharp.Domain
 {
     public class HistoricDeposit
     {
-        public string PaymentUuid { get; set; }
-        public string Currency { get; set; }
+        public long Id { get; set; }
         public decimal Amount { get; set; }
-        public string Address { get; set; }
-        public DateTime Opened { get; set; }
-        public bool Authorized { get; set; }
-        public bool PendingPayment { get; set; }
-        public decimal TxCost { get; set; }
+        public string Currency { get; set; }
+        public int Confirmations { get; set; }
+        public DateTime LastUpdated { get; set; }
         public string TxId { get; set; }
-        public bool Canceled { get; set; }
-        public bool InvalidAddress { get; set; }
+        public string CryptoAddress { get; set; }
     }
 }
